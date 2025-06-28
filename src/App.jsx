29fc7ed6b4e26441ced5e814155def97017dee68
maheswar <maheswarreddyappojigari@gmail.com>
2025-06-28 16:22:26 +0530

    added new components(static ui),in order to get overview of features and components of the app,and added some browser routes using react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import React from "react"
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import AppLayout from "./ui/AppLayout";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {index:true,element:<Home/>},
+      { path: "menu", element: <Menu /> },
+      { path: "cart", element: <Cart /> },
+      { path: "cart", element: <Cart /> },
+      { path: "order/new", element: <CreateOrder /> },
+      { path: "order/:orderId", element: <Order /> },
+    ],
+  },
+]);
 function App() {
-  return (
-    <div>
-      Hello vite
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router"
+
+function Header() {
+    return (
+        <header>
+            <Link to="/">Fast React Pizza co</Link>
+            
+        </header>
+    )
+}
+
+export default Header
